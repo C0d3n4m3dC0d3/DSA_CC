0d27d15df867655ab24915b58738872f3820ba95
Sreenidhi B S <47050310+C0d3n4m3dC0d3@users.noreply.github.com>
2021-06-09 06:39:07 +0530

    Add files via upload

--- binary_tree.py
+++ binary_tree.py
@@ -1,28 +1,29 @@
-class BT: # node
+class BT:
 	def __init__(self, data):
 		self.data = data
 		self.left = None
 		self.right = None
 
-def print_tree(t, level):
+def print_tree(t, lvl):
 	if t == None:
-		if level == 0:
-			print("Tree is Empty")
-			return
+		if lvl == 0:
+			print("Empty Tree")
 		else:
 			return
-	for i in range(level):
-		print(" ", end = "")
-
-	print(t.data)
-	print_tree(t.left, level+1)
-	print_tree(t.right, level+1)
-
-def height(tree):
-	if tree == None:
-		return 0
 	else:
-		return (1 + max(height(tree.left), height(tree.right)))
+		for i in range(lvl):
+			print(' ', end='')
+		print(t.data)
+		print_tree(t.left, lvl+1)
+		print_tree(t.right, lvl+1)
+
+def height(t):
+	if t == None:
+		return 0
+	count = 0
+	count = max(count, height(t.left))
+	count = max(count, height(t.right))
+	return count+1
 
 if __name__ == '__main__':
 	a = BT('A')
@@ -31,17 +32,16 @@ if __name__ == '__main__':
 	d = BT('D')
 	e = BT('E')
 	f = BT('F')
-
 	a.left = b
 	b.left = c
 	b.right = d
 	c.left = e
 	d.right = f
-
 	print_tree(a, 0)
 	print_tree(None, 0)
 
-	print(height(None))
-	print(height(a))
-	print(height(c))
-	print(height(b))
+	print(height(None)) #0
+	print(height(a)) #4
+	print(height(b)) #3
+	print(height(c)) #2
+	print(height(f)) #1
